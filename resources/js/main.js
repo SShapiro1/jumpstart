a347f87b76532ebce94cdf3ba57535a3fe83f6b6
SShapiro1 <StuartShapiro1@gmail.com>
2017-05-24 08:12:55 -0400

    carousel added

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel .jumbotron').slick({
+    autoplay: true,
+    slidestoScroll:1,
+    arrows: false,
+    centerMode: true,
+    autoplaySpeed:4000,
+
+  });
+});
