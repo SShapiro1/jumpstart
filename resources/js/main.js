adb786b3982fed3ae4570d7472a292f364c1eb01
SShapiro1 <StuartShapiro1@gmail.com>
2017-05-24 23:09:56 -0400

    added slick and jquery

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
-  $('.carousel .jumbotron').slick({
-    autoplay: true,
-    slidestoScroll:1,
-    arrows: false,
-    centerMode: true,
-    autoplaySpeed:4000,
 
-  });
+<script type="text/javascript">
+$(document).ready(function(){
+$('background').slick({
+  arrows: false,
 });
+});
+</script>
